cfde02c7accbf4c58f6a970ae6aa7a5b476d0ee8
p-and14 <p-and14@yandex.ru>
2025-06-09 20:58:23 +0300

    v2

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import random
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
 
+
 TASKS_COUNT = 100
-WORKERS_COUNT = 10
+WORKERS_COUNT = 5
 COUNT_SIMULTANEOUS_TASKS = 5
-TASKS_WITH_ERROR = []
 
 
 async def create_queue_with_tasks(count: int) -> asyncio.Queue:
     "Создаёт очередь с заданным количеством задач"
+
     queue = asyncio.Queue()
 
     for i in range(1, count + 1):
@@ -25,34 +26,39 @@ async def create_queue_with_tasks(count: int) -> asyncio.Queue:
 
 
 async def worker(queue: asyncio.Queue, semaphore: asyncio.Semaphore) -> None:
-    "Выполняет задачи, до последней задачи в очереди"
+    "Выполняет задачи из очереди в Semaphore"
+
     async with semaphore:
-        while not queue.empty():
+        while True:
             task: dict[str, int | float] = await queue.get()
             duration = task.get("duration", 0)
             task_id = task.get("task_id")
 
             log.info(f"Начало выполнении задачи с id={task_id}")
+            await asyncio.sleep(duration)
+            queue.task_done()
+            log.info(f"Задача с id={task_id} выполнена")
+                
 
-            try:
-                await asyncio.sleep(duration)
-                if duration < 0.6:
-                    raise ValueError("Low duration")
-                log.info(f"Задача с id={task_id} выполнена")
-                queue.task_done()
-            except Exception as e:
-                log.error(f"Ошибка при выполнении задачи с id={task_id}: {e}")
-                TASKS_WITH_ERROR.append((task, e))
-
-
-async def main():
+async def main() -> None:
     """Создаёт очередь и запускает воркеров в количестве WORKERS_COUNT.
     Одновременно выполняется не более COUNT_SIMULTANEOUS_TASKS задач"""
+
     queue = await create_queue_with_tasks(TASKS_COUNT)
     semaphore = asyncio.Semaphore(COUNT_SIMULTANEOUS_TASKS)
-    await asyncio.gather(*[worker(queue, semaphore) for _ in range(WORKERS_COUNT)])
+    workers = [worker(queue, semaphore) for _ in range(WORKERS_COUNT)]
+    tasks: list[asyncio.Task] = []
+
+    for w in workers:
+        tasks.append(asyncio.create_task(w))
+    
+    await queue.join()
+
+    for task in tasks:
+        task.cancel()
+    
+    await asyncio.gather(*tasks, return_exceptions=True)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
-    print(TASKS_WITH_ERROR)
